3de2ebc3aafe6e8b47451ce25ac8255158130108
Larisa <larisabainton@gmail.com>
2016-10-04 11:42:17 -0400

    Recipe edited page passes all tests

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -24,6 +24,19 @@ class RecipesController < ApplicationController
   end
 
   def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    if @recipe.update_attributes(recipe_params)
+      flash[:notice] = "Recipe edited successfully"
+      redirect_to @recipe
+    else
+      flash[:notice] = @recipe.errors.full_messages.join(', ')
+      render 'edit'
+    end
   end
 
   def destroy
