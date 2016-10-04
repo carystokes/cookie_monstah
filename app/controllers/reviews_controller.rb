82bacdbd917778bc1edfcfe9761837cf4ecc0d75
Larisa <larisabainton@gmail.com>
2016-10-04 15:12:28 -0400

    all review tests passing

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,9 +3,14 @@ class ReviewsController < ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
     @review = Review.new(review_params)
     @review.recipe = @recipe
-    @review.save
-    flash[:notice] = "Review created successfully"
-    redirect_to recipe_path(@recipe)
+
+    if @review.save
+      flash[:notice] = "Review created successfully"
+      redirect_to recipe_path(@recipe)
+    else
+      flash[:notice] = @review.errors.full_messages.join(", ")
+      redirect_to recipe_path(@recipe)
+    end
   end
 
   private
--- spec/features/user_reviews_recipes_spec.rb
+++ spec/features/user_reviews_recipes_spec.rb
@@ -38,19 +38,46 @@ feature "User writes a review" do
     # scenario "I must be signed in to create a review" do
     # end
 
-    scenario "I must a rating to create a review and optionally a description" do
+    scenario "I must have a rating to create a review and optionally a description" do
       visit recipe_path(recipe)
 
+      fill_in "Rating", with: 4
+
+      click_button "Add a Review"
+
+      expect(page).to have_content "Review created successfully"
+      expect(page).to have_content "Rating: 4"
     end
 
-    scenario "I can provide only a rating and still create a review" do
+    scenario "I expect an error if I put a rating that is not between 1 and 5" do
       visit recipe_path(recipe)
 
+      fill_in "Rating", with: 27
+      fill_in "Body", with: "THIS IS THE BEST COOKIE SOUP EVER!!!!!"
+
+      click_button "Add a Review"
+
+      expect(page).to have_content "Rating must be between 1 - 5"
+    end
+
+    scenario "I expect an error if I put a rating that is not an integer" do
+      visit recipe_path(recipe)
+
+      fill_in "Rating", with: 3.7
+
+      click_button "Add a Review"
+
+      expect(page).to have_content "Rating must be an integer"
     end
 
     scenario "I expect an error if I don't provide a rating" do
       visit recipe_path(recipe)
 
+      fill_in "Body", with: "I don't like numbers I'd rather just use my words."
+
+      click_button "Add a Review"
+
+      expect(page).to have_content "Rating is not a number"
     end
   end
 end
