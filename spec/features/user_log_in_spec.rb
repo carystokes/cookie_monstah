eeea0b31eae72963d7e67447af7bf7d7c50e1341
carystokes <cary.p.stokes@gmail.com>
2016-10-04 15:43:27 -0400

    update to use factory girl gem

--- spec/features/user_log_in_spec.rb
+++ spec/features/user_log_in_spec.rb
@@ -4,18 +4,10 @@ require 'pry'
 feature 'log in' do
   context 'as a user' do
     scenario 'user provides valid user information for log-in' do
-      visit new_user_registration_path
-      fill_in 'First name', with: 'Jon'
-      fill_in 'Last name', with: 'Smith'
-      fill_in 'Email', with: 'user@example.com'
-      fill_in 'Password', with: 'password'
-      fill_in 'Password confirmation', with: 'password'
-      click_button 'Sign Up'
-      click_link 'Log Off'
-      expect(page).to have_content("Signed out successfully.")
+      jon = FactoryGirl.create(:user)
 
       visit new_user_session_path
-      fill_in 'Email', with: 'user@example.com'
+      fill_in 'Email', with: jon.email
       fill_in 'Password', with: 'password'
 
       click_button 'Log in'
