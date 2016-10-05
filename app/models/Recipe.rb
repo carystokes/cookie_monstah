eaf2706858ff4ddcd2721ee9bf27364ff5ad65bf
Larisa <larisabainton@gmail.com>
2016-10-05 09:48:17 -0400

    tried changing the order of the lines in the files so git will recognize the files are lowercase

--- app/models/Recipe.rb
+++ app/models/Recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ActiveRecord::Base
   belongs_to :user
   has_many :reviews
 
+  validates :ingredients, presence: true
   validates :title, presence: true
   validates :instructions, presence: true
-  validates :ingredients, presence: true
 end
--- app/models/Review.rb
+++ app/models/Review.rb
@@ -1,6 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :recipe
 
-  validates :rating, numericality: {only_integer: true},
-   inclusion: {in: 1..5, message: "Rating must be between 1 - 5"}
+  validates :rating, numericality: {only_integer: true}
+  validates :rating, inclusion: {in: 1..5,
+    message: "Rating must be between 1 - 5"
+  }, 
 end
