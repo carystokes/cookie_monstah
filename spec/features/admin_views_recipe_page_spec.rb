03af6993b26cde4422221e2b876ee0ee08e170bd
Shana <shanajeanhausman@gmail.com>
2016-10-07 14:09:36 -0400

    finished set of admin tests

--- spec/features/admin_views_recipe_page_spec.rb
+++ spec/features/admin_views_recipe_page_spec.rb
@@ -6,15 +6,26 @@ feature 'admin views all users do' do
   let!(:user2) {FactoryGirl.create(:user, name: 'Joan', admin: true)}
   context 'as an admin' do
     feature 'an admin can delete recipes' do
+      user_sign_in(user2)
+      visit recipe_path(recipe)
+      click_link 'Delete Recipe'
 
+      expect(page).not_to have_content(recipe.title)
+      expect(page).to have_content 'Recipe deleted'
     end
 
     feature 'there is no link for an admin to edit recipes' do
+      user_sign_in(user2)
+      visit recipe_path(recipe)
 
+      expect(page).not_to have_link 'Edit Recipe'
     end
 
     feature 'an admin cannot edit recipes' do
-      
+      user_sign_in(user2)
+      visit edit_recipe_path(recipe)
+
+      expect(page).to have_content('You cannot edit this recipe')
     end
   end
 end
