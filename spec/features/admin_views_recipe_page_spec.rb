82e13ac0a2a872f85d0da799ac8637b3501c891c
Shana <shanajeanhausman@gmail.com>
2016-10-07 11:10:29 -0400

    working on the test

--- spec/features/admin_views_recipe_page_spec.rb
+++ spec/features/admin_views_recipe_page_spec.rb
@@ -1,1 +1,20 @@
 require 'rails_helper'
+
+feature 'admin views all users do' do
+  let!(:recipe) { FactoryGirl.create(:recipe) }
+  let!(:user) { recipe.user }
+  let!(:user2) {FactoryGirl.create(:user, name: 'Joan', admin: true)}
+  context 'as an admin' do
+    feature 'an admin can delete recipes' do
+
+    end
+
+    feature 'there is no link for an admin to edit recipes' do
+
+    end
+
+    feature 'an admin cannot edit recipes' do
+      
+    end
+  end
+end
