cd9ec5e36a7fad04694212a22a37fd187be82305
mhawley1230@gmail.com <Mike>
2016-10-12 13:27:06 -0400

    yet even more hound edits

--- lib/carrierwave/storage/fog.rb
+++ lib/carrierwave/storage/fog.rb
@@ -400,7 +400,7 @@ module CarrierWave
         #
         def filename(options = {})
           return unless file_url == url(options)
-          CGI.unescape(file_url.split('?').first).gsub(/.*\/(.*?$)/, '\1')
+          CGI.unescape(file_url.split('?').first).gsub(%r{.*\/(.*?$)}, '\1')
         end
 
         ##
