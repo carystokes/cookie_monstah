dd16292281a08fa3760c61d5c03a1e30e147ee30
mhawley1230@gmail.com <Mike>
2016-10-13 15:30:07 -0400

    fixed double quotes to single

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,31 @@
 User.create!(
-  first_name: "Mike",
-  last_name: "Hawley",
-  email: "mhawley1230@gmail.com",
-  password: "password",
+  first_name: 'Mike',
+  last_name: 'Hawley',
+  email: 'mhawley1230@gmail.com',
+  password: 'password',
   admin: true
 )
 
 User.create!(
-  first_name: "Shana",
-  last_name: "Hausman",
-  email: "shanajeanhausman@gmail.com",
-  password: "password",
+  first_name: 'Shana',
+  last_name: 'Hausman',
+  email: 'shanajeanhausman@gmail.com',
+  password: 'password',
   admin: true
 )
 
 User.create!(
-  first_name: "Larisa",
-  last_name: "Bainton",
-  email: "larisabainton@gmail.com",
-  password: "password",
+  first_name: 'Larisa',
+  last_name: 'Bainton',
+  email: 'larisabainton@gmail.com',
+  password: 'password',
   admin: true
 )
 
 User.create!(
-  first_name: "Cary",
-  last_name: "Stokes",
-  email: "cary.p.stokes@gmail.com",
-  password: "password",
+  first_name: 'Cary',
+  last_name: 'Stokes',
+  email: 'cary.p.stokes@gmail.com',
+  password: 'password',
   admin: true
 )
