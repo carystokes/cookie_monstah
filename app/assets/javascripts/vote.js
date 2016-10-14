4462e68ba92bfb89428462addc56bb3db8b2e65a
Shana <shanajeanhausman@gmail.com>
2016-10-14 13:13:30 -0400

    voting works now

--- app/assets/javascripts/vote.js
+++ app/assets/javascripts/vote.js
@@ -1,35 +1,3 @@
-// $(document).ready(function() {
-//   debugger;
-//   $('.upvote').on('click', function(event) {
-//     event.preventDefault();
-//
-//     let url = this.parentElement.action;
-//     debugger;
-//     let currentTarget = $(event.currentTarget);
-//     let id = parseInt(this.id);
-//
-//     let request = $.ajax({
-//       method: "POST",
-//       url: `${url}.json`
-//     });
-//
-//     request.done(function(data) {
-//       document.getElementById(`total${id}`).innerHTML = data.vote;
-//     })
-//   });
-//
-//   $('.downvote').on('click', function(event) {
-//     event.preventDefault();
-//
-//     let request = $.ajax({
-//       method: "POST",
-//       url: "/review/id/downvote"
-//     });
-//
-//     request.done()
-//   });
-// })
-
 function upvoteButton() {
   event.preventDefault();
   let id = event.currentTarget.id;
